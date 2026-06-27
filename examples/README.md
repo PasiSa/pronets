ab8f131dff67e411d401aeaa32f963d71c9941cf
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2026-06-27 12:06:31 +0300

    Add simple-client example

--- examples/README.md
+++ examples/README.md
@@ -3,4 +3,20 @@
 
 # List of examples
 
-_TBD_
+This folder contains Rust network programming examples referred to in the course
+material, each in separate Rust project folder. They contain Cargo.toml, etc.
+required files so that you can build and test them using `cargo build` and
+`cargo run`.
+
+One easy way to try the examples is that you clone the course repository to your
+machine and locate this directory. The examples contain Cargo.toml, etc.
+required files so that you can build and test them using `cargo build` and
+`cargo run`. Feel free to modify the examples as you want, to try different
+things.
+
+The examples are as follows:
+
+- **[simple-client](https://github.com/PasiSa/pronets/tree/main/examples/simple-client/src/main.rs)**:
+  Opens connection, then writes and reads a bit of data.
+
+- _TODO: more examples to follow_
---
+++ examples/simple-client/src/main.rs
@@ -0,0 +1,46 @@
+/* Open TCP socket, send some data and receive data back.
+ *
+ * Usage: cargo run -- <name/address>:<port> <string>
+ */
+
+use std::{
+    env,
+    io::{self, Read, Write},
+    net::TcpStream,
+};
+
+fn main() -> io::Result<()> {
+    // Collect command-line arguments into a vector
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("arguments: <host>:<port> <message>");
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Invalid command",
+        ));
+    }
+
+    // Connect TCP socket. If connection fails, exit the main function with error.
+    // The connect function does both name resolution and connection establishment.
+    let mut socket = TcpStream::connect(&args[1])?;
+
+    // as_bytes() converts the string from command line argument into a u8 byte slice.
+    // If write produces an error, we exit the main function immediately.
+    let n = socket.write(args[2].as_bytes())?;
+    if n < args[2].len() {
+        // We could not write everything.
+        // In reality, we should call write() again with the rest of the data.
+        println!("Not everything was written");
+    }
+
+    // Allocate u8 buffer of 160 bytes and read data to it from socket.
+    // Print the output to screen.
+    // Interpret as UTF-8, replacing invalid bytes instead of returning an error.
+    // If read causes error, exits the main function
+    let mut buf: [u8; 160] = [0; 160];
+    socket.read(&mut buf)?;
+    println!("{}", String::from_utf8_lossy(&buf));
+
+    Ok(()) // Everything successful!
+}
