01c96a52b61d3934d0dc64e39b80657d94f8c794
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2026-09-16 13:05:50 +0300

    Fix proper error handling to simple-server example

--- examples/simple-server/src/main.rs
+++ examples/simple-server/src/main.rs
@@ -28,19 +28,57 @@ fn main() -> io::Result<()> {
     loop {
         // Wait until new connection request comes in.
         // accept returns active socket and address of the connecting host as tuple.
-        let (mut socket, address) = server.accept()?;
+        // Accept may fail for different reasons: if client aborted connection,
+        // we can continue.
+        // Other kinds of errors may be more persistent (out of file descriptors, etc.),
+        // so then we exit the main function with error.
+        let (mut socket, address) = match server.accept() {
+            Ok((socket, address)) => (socket, address),
+            Err(e) if e.kind() == ErrorKind::ConnectionAborted => {
+                eprintln!("Client aborted connection: {e}");
+                continue;
+            }
+            Err(e) => return Err(e),
+        };
         println!("Accepting connection from {}", address.to_string());
 
         // Read at most 160 bytes from the established connection
         // 'readn' will contain the number of bytes actually read.
-        // If read fails, the error causes main function to exit.
+        // If the client closes or aborts the connection, accept the next client.
+        // Other read errors cause main to exit.
         let mut buf: [u8; 160] = [0; 160];
-        let readn = socket.read(&mut buf)?;
+        let readn = match socket.read(&mut buf) {
+            Ok(readn) => {
+                if readn == 0 {
+                    println!("Client closed connection");
+                    continue;
+                }
+                readn
+            }
+            Err(e) => {
+                eprintln!("Error reading socket: {e}");
+                continue;
+            }
+        };
 
         // Write the bytes that were read back to the client.
         // 'writen' will contain the number of bytes actually written.
         // If function fails, the error causes main function to exit.
-        let writen = socket.write(&buf[..readn])?;
+        // Write only as many bytes as were read, because the buffer may not be full.
+        // For output, we convert the bytes to a string using from_utf8_lossy,
+        // which will replace invalid UTF-8 sequences with the replacement character.
+        println!(
+            "Read {} bytes: {}",
+            readn,
+            String::from_utf8_lossy(&buf[..readn])
+        );
+        let writen = match socket.write(&buf[..readn]) {
+            Ok(writen) => writen,
+            Err(e) => {
+                eprintln!("Write error: {e}");
+                continue;
+            }
+        };
         println!("Wrote {} bytes", writen);
 
         // Client socket is implicitly closed as 'socket' goes out of scope
